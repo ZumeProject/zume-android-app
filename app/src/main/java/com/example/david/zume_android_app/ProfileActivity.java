33f6fc6b9b0a69f04042ca1ce91d6a89b055fa6b
daoffner <dpoffner@sbcglobal.net>
2018-02-24 15:29:15 -0600

    Merge branch 'master' into david

--- app/src/main/java/com/example/david/zume_android_app/ProfileActivity.java
+++ app/src/main/java/com/example/david/zume_android_app/ProfileActivity.java
@@ -1,15 +1,26 @@
 package com.example.david.zume_android_app;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private String resultFromAPI = "";
+    private String baseUrl = "";
+    private String username = "";
+    private String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +42,127 @@ public class ProfileActivity extends AppCompatActivity {
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ProfileActivity.this, DashboardActivity.class));
+
+                Intent intent = getIntent();
+                String username = intent.getStringExtra("username");
+                String password = intent.getStringExtra("password");
+
+                Bundle bundle = new Bundle();
+                bundle.putString("username", username);
+                bundle.putString("password", password);
+
+                intent = new Intent(ProfileActivity.this, DashboardActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
+
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+        password = intent.getStringExtra("password");
+
+        baseUrl = "http://zume.hsutx.edu/wp-json/zume/v1/android/user_profile/1";
+        try {
+
+            ApiAuthenticationClient apiAuthenticationClient =
+                    new ApiAuthenticationClient(
+                            baseUrl
+                            , username
+                            , password
+                    );
+
+            AsyncTask<Void, Void, String> execute = new ProfileActivity.ExecuteNetworkOperation(apiAuthenticationClient);
+            execute.execute();
+        } catch (Exception ex){
+            Log.d("Test","Error getting profile data.");
+        }
+
+    }
+
+    /**
+     * This subclass handles the network operations in a new thread.
+     * It starts the progress bar, makes the API call, and ends the progress bar.
+     */
+    public class ExecuteNetworkOperation extends AsyncTask<Void, Void, String> {
+
+        private ApiAuthenticationClient apiAuthenticationClient;
+
+        /**
+         * Overload the constructor to pass objects to this class.
+         */
+        public ExecuteNetworkOperation(ApiAuthenticationClient apiAuthenticationClient) {
+            this.apiAuthenticationClient = apiAuthenticationClient;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Display the progress bar.
+            //findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                Log.d("Test", "Made to the execute method");
+                resultFromAPI = apiAuthenticationClient.execute();
+            } catch (Exception e) {
+                Log.d("Test", "Error making it to execute method");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            // Hide the progress bar.
+            //findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+
+            // Credentials correct
+            if (resultFromAPI != null && !resultFromAPI.equals("")) {
+                setProfileScreen();
+            }
+            // Login Failure
+            else {
+                Toast.makeText(getApplicationContext(), "Error opening profile: Invalid Credentials", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     * Open a new activity window.
+     */
+    private void setProfileScreen() {
+        try{
+            JSONObject reader = new JSONObject(resultFromAPI);
+            JSONArray first = reader.getJSONArray("first_name");
+            JSONArray last = reader.getJSONArray("last_name");
+            JSONArray nickname = reader.getJSONArray("nickname");
+            JSONArray lastActive = reader.getJSONArray("zume_last_active");
+
+            TextView name = (TextView)findViewById(R.id.name);
+            if(!first.get(0).equals("") || !last.get(0).equals("")){
+                name.setText(first.get(0) + " " + last.get(0));
+            }
+            else{
+                name.setText(nickname.get(0).toString());
+            }
+
+            TextView activity = (TextView)findViewById(R.id.lastActivity);
+            if(!lastActive.get(0).equals("")){
+                activity.setText("Last Active: " + lastActive.get(0));
+            }
+
+
+        }
+        catch(Exception e) {
+
+        }
+
+        TextView home = (TextView)findViewById(R.id.home);
     }
 
 }
