f0c80bd950296dd44d22d76d577733318f601f3b
daoffner <dpoffner@sbcglobal.net>
2018-04-24 13:00:20 -0500

    Ficed the login for no internet

--- app/src/main/java/com/example/david/zume_android_app/GetUser.java
+++ app/src/main/java/com/example/david/zume_android_app/GetUser.java
@@ -184,9 +184,17 @@ public class GetUser extends AppCompatActivity {
                         Log.d("Test", "Opened the file");
                         InputStreamReader isr = new InputStreamReader(fis);
                         BufferedReader bufferedReader = new BufferedReader(isr);
+                        String user = "", pass = "", tok ="";
                         try {
-                            username = bufferedReader.readLine();
-                            password = bufferedReader.readLine();
+                            user = bufferedReader.readLine();
+                            pass = bufferedReader.readLine();
+                            bufferedReader.readLine();
+                            tok = bufferedReader.readLine();
+                            if (user.equals(username) && pass.equals(password)) {
+                                username = user;
+                                password = pass;
+                            }
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
